fd114df2affa8e869f1f07b6b0705da9e0dcecff
Sören Tempel <soeren+git@soeren-tempel.net>
2026-07-28 18:26:51 +0200

    .guix: Update Guix channel for qbe v1.3 support

--- .guix/channels.scm
+++ .guix/channels.scm
@@ -7,7 +7,7 @@
         (url "https://git.guix.gnu.org/guix.git")
         (branch "master")
         (commit
-          "b25112491eba874a739ce2916d563ddb6d1b200c")
+          "2c343b81361f18d283c1eb36061b14c0021756ed")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
