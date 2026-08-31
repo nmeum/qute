81abbd2d38169d53c64be30dab04b7715685aa8a
Sören Tempel <soeren+git@soeren-tempel.net>
2026-08-31 12:10:30 +0200

    qute-symex: Skip tests if no solver was found
    Similar to the prior qute-syntax change: Hackage tries to run the
    run the tests but Hackage doesn't have any solvers.

--- qute-symex/test/Main.hs
+++ qute-symex/test/Main.hs
@@ -1,4 +1,4 @@
--- SPDX-FileCopyrightText: 2025 Sören Tempel <soeren+git@soeren-tempel.net>
+-- SPDX-FileCopyrightText: 2025-2026 Sören Tempel <soeren+git@soeren-tempel.net>
 --
 -- SPDX-License-Identifier: GPL-3.0-only
 
@@ -7,13 +7,22 @@ module Main (main) where
 import BV (bvTests)
 import Backend (backendTests)
 import Concolic qualified as CE
+import Control.Exception (IOException, try)
+import Data.Either (isLeft)
 import Explorer (exploreTests)
 import Golden (goldenTests)
+import Language.QBE.Simulator.Explorer (defSolver)
+import SimpleBV qualified as SMT
 import Symbolic qualified as SE
+import System.IO (hPutStrLn, stderr)
 import Test.Tasty
 
 main :: IO ()
-main = defaultMain tests
+main = do
+  solver <- try defSolver :: IO (Either IOException SMT.Solver)
+  if isLeft solver
+    then hPutStrLn stderr "WARNING: No solver found, skipping tests!"
+    else defaultMain tests
 
 tests :: TestTree
 tests =
